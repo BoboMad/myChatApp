6778f4c24600bcefac8de305cb59b52617fa448b
Mathias Forsbrg <mathias@forsberg.cc>
2024-09-27 15:34:23 +0200

    first commit

--- myChatApp.Server/Program.cs
+++ myChatApp.Server/Program.cs
@@ -7,10 +7,7 @@ namespace myChatApp.Server
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
@@ -19,7 +16,6 @@ namespace myChatApp.Server
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
